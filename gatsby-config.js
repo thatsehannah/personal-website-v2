64290586411fb763b59c80662900b23328f7bcc5
E. Hannah <echannah631@gmail.com>
2023-05-23 08:11:29 -0500

    Initial commit from gatsby: (https://github.com/mengto/gatsby-starter-designcode.git)

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+ module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/images/icons/favicon-32x32.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
