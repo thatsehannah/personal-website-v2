7072c6bedeaaa0fa348b16e5d84822466206c140
E. Hannah <echannah631@gmail.com>
2023-06-14 21:57:12 -0700

    forgot seo

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout/layout"
+import Seo from "../components/layout/seo"
 
 const Portfolio = () => {
   return (
     <Layout>
+      <Seo title="Portfolio" />
       <div>I'm the Portfolio page</div>
     </Layout>
   )
