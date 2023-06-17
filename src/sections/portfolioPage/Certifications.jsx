9d77e84d42638a4915e6fe2b16ced2da87844934
E. Hannah <echannah631@gmail.com>
2023-06-17 12:02:46 -0500

    improved certs layout

--- src/sections/portfolioPage/Certifications.jsx
+++ src/sections/portfolioPage/Certifications.jsx
@@ -15,10 +15,23 @@ const Certifications = () => {
             <Cert src={node.url} />
           </CertWrapper>
           <TextWrapper>
-            <BodyIntro>
+            <Title>
               {node.title} from {node.from}
-            </BodyIntro>
+            </Title>
             <BodyMain>Received on {node.issueDate}</BodyMain>
+            <List>
+              <ListItem>
+                Ornare lectus sit amet est placerat in egestas erat imperdiet.
+              </ListItem>
+              <ListItem>
+                Cras tincidunt lobortis feugiat vivamus at augue eget arcu
+                dictum.
+              </ListItem>
+              <ListItem>Posuere mori leo urna molestie at.</ListItem>
+              <ListItem>
+                Aliquet bibendu enfacilisis gravida neque convallis a.
+              </ListItem>
+            </List>
           </TextWrapper>
         </ContentWrapper>
       ))}
@@ -30,13 +43,13 @@ export default Certifications
 
 const Wrapper = styled.div`
   display: grid;
-  gap: 50px;
+  gap: 80px;
 `
 
 const ContentWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 20px;
+  gap: 41px;
 
   &:nth-child(even) {
     flex-direction: row-reverse;
@@ -45,23 +58,44 @@ const ContentWrapper = styled.div`
 
 const CertWrapper = styled.div`
   display: flex;
+  flex: 1;
   justify-content: center;
   align-items: center;
   background-color: rgba(0, 147, 233, 0.2);
+
   border: 0.5px solid black;
-  width: 600px;
+  border-radius: 24px;
+  width: 550px;
+  height: 440px;
   padding: 20px;
 `
 
 const Cert = styled.img`
   height: 90%;
-  width: 100%;
+  width: 90%;
   transition: 0.5s ease-in-out;
 
   :hover {
     cursor: pointer;
-    transform: scale(1.05);
+    transform: scale(1.1);
   }
 `
 
-const TextWrapper = styled.div``
+const TextWrapper = styled.div`
+  flex: 1;
+`
+
+const Title = styled(BodyIntro)`
+  margin-bottom: 25px;
+  font-weight: 500;
+`
+
+const List = styled.ul`
+  list-style-type: disc;
+  padding: 12px;
+`
+
+const ListItem = styled.li`
+  font: ${BodyMain};
+  padding: 10px;
+`
