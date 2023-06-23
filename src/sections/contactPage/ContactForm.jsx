8a383eff183e245cd2720aeb4f654bc369204cd4
E. Hannah <echannah631@gmail.com>
2023-06-23 00:27:48 -0500

    contact form is done, using axios instead of fetch

--- src/sections/contactPage/ContactForm.jsx
+++ src/sections/contactPage/ContactForm.jsx
@@ -1,63 +1,175 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import styled from "styled-components"
 import MainButton from "../../components/buttons/MainButton"
-import { H1, H2 } from "../../styles/TextStyles"
+import { H1 } from "../../styles/TextStyles"
+import axios from "axios"
+import Letter from "../../components/special/Letter"
+import { fadeInAnimation } from "../../styles/FadeInAnimation"
 
 const ContactForm = () => {
-  const [details, setDetails] = useState({ name: "", email: "", message: "" })
+  const [formData, setFormData] = useState({ name: "", email: "", message: "" })
+  const [errors, setErrors] = useState({ name: "", email: "", message: "" })
+  const [toast, setToast] = useState({
+    show: false,
+    message: "",
+    backgroundColor: "",
+  })
 
-  const handleInputChange = event => {
-    setDetails({ ...details, [event.target.name]: event.target.value })
+  useEffect(() => {
+    if (toast.show) {
+      const timer = setTimeout(() => {
+        setToast({ show: false, message: "", backgroundColor: "" })
+      }, 5000)
+      return () => clearTimeout(timer)
+    }
+  }, [toast.show])
+
+  const resetFormData = () => {
+    setFormData({ name: "", email: "", message: "" })
+    setErrors({ name: "", email: "", message: "" })
   }
 
-  const handleSubmit = event => {
-    console.log(details)
+  const handleInputChange = event => {
+    setFormData({ ...formData, [event.target.name]: event.target.value })
+    validateInput(event.target.name, event.target.value)
+  }
+
+  const validateInput = (input, value) => {
+    let err = ""
+    switch (input) {
+      case "name":
+        if (!value.trim()) {
+          err = "Please provide a name."
+        }
+        break
+      case "email":
+        const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/
+        if (!emailRegex.test(value)) {
+          err = "Please provide a valid email address."
+        }
+        break
+      case "message":
+        if (!value.trim()) {
+          err = "Please provide a message."
+        }
+        break
+      default:
+        break
+    }
+
+    setErrors(prevErrors => ({ ...prevErrors, [input]: err }))
+  }
+
+  const checkIsFormValid = () => {
+    return (
+      formData.name === "" ||
+      formData.message === "" ||
+      formData.email === "" ||
+      errors.email !== ""
+    )
+  }
+
+  const handleSubmit = async event => {
     event.preventDefault()
 
-    fetch("https://us-central1-thatsehannah.cloudfunctions.net/sendMail", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(details),
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log("Success:", data)
+    try {
+      const res = await axios.post(process.env.GATSBY_FIREBASE_FUNCTION_URL)
+
+      if (res.data.isEmailSend) {
+        setToast({
+          show: true,
+          message: `Thanks! A response was sent to ${formData.email}`,
+          backgroundColor: "rgba(80, 200, 120, 0.8)",
+        })
+      } else {
+        setToast({
+          show: true,
+          message: "An error occurred sending your message.",
+          backgroundColor: "rgba(196, 30, 58, 0.8)",
+        })
+      }
+    } catch (error) {
+      setToast({
+        show: true,
+        message: `There was an error sending the message: ${error}`,
+        backgroundColor: "rgba(196, 30, 58, 0.8)",
       })
-      .catch(err => {
-        console.error("Error:", err)
-      })
+      console.error("There was an error sending the message: ", error)
+    } finally {
+      resetFormData()
+    }
   }
 
   return (
     <Wrapper>
       <ContentWrapper>
-        <H1>Let's Connect!</H1>
-        <Form onSubmit={handleSubmit}>
-          <Label htmlFor="name">Name</Label>
-          <Input
-            id="name"
-            type="text"
-            name="name"
-            onChange={handleInputChange}
-          />
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            type="email"
-            name="email"
-            onChange={handleInputChange}
-          />
-          <Label htmlFor="message">Message</Label>
-          <MessageInput
-            id="message"
-            type="text"
-            name="message"
-            onChange={handleInputChange}
-          />
+        <Toast show={toast.show} backgroundColor={toast.backgroundColor}>
+          {toast.message}
+        </Toast>
+        <TitleWrapper>
+          <TopWrapper>
+            <Memoji src="/images/memojis/hero-memoji.png" />
+            <EmojiSpan>➕</EmojiSpan>
+            <EmojiSpan>🫵</EmojiSpan>
+          </TopWrapper>
+          <Title>
+            Let's chop it up. Send m<Letter /> a message...
+          </Title>
+        </TitleWrapper>
+
+        <Form>
+          <InputWrapper>
+            <Label htmlFor="name">Name</Label>
+            <Input
+              id="name"
+              type="text"
+              name="name"
+              value={formData.name}
+              onBlur={() => validateInput("name", formData.name)}
+              onChange={handleInputChange}
+            />
+            <ErrorWrapper>
+              {errors.name && <ErrorText>{errors.name}</ErrorText>}
+            </ErrorWrapper>
+          </InputWrapper>
+
+          <InputWrapper>
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              value={formData.email}
+              onBlur={() => validateInput("email", formData.email)}
+              onChange={handleInputChange}
+            />
+            <ErrorWrapper>
+              {errors.email && <ErrorText>{errors.email}</ErrorText>}
+            </ErrorWrapper>
+          </InputWrapper>
+
+          <InputWrapper>
+            <Label htmlFor="message">Message</Label>
+            <MessageInput
+              id="message"
+              type="text"
+              name="message"
+              value={formData.message}
+              onBlur={() => validateInput("message", formData.message)}
+              onChange={handleInputChange}
+            />
+            <ErrorWrapper>
+              {errors.message && <ErrorText>{errors.message}</ErrorText>}
+            </ErrorWrapper>
+          </InputWrapper>
+
           <ButtonWrapper>
-            <button type="submit">Submit</button>
+            <MainButton
+              onClick={handleSubmit}
+              type="submit"
+              text="Send message"
+              disabled={checkIsFormValid()}
+            />
           </ButtonWrapper>
         </Form>
       </ContentWrapper>
@@ -71,7 +183,6 @@ const Wrapper = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 60px 20px;
 
   @media (max-width: 768px) {
     padding: 0px 20px 100px;
@@ -82,7 +193,7 @@ const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 1400px;
-  margin: 0 auto;
+  /* margin: 0 auto; */
   gap: 60px;
 
   @media (max-width: 768px) {
@@ -91,28 +202,104 @@ const ContentWrapper = styled.div`
   }
 `
 
-const Form = styled.form`
+const Toast = styled.div`
+  position: fixed;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: ${props => props.backgroundColor};
+  color: #fff;
+  padding: 16px;
+  border-radius: 4px;
+  z-index: 8;
+  opacity: ${props => (props.show ? "1" : "0")};
+  transition: opacity 0.5s ease;
+`
+
+const TopWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 48px;
+  overflow: hidden;
+`
+
+const TitleWrapper = styled.div`
+  display: grid;
+  gap: 10px;
+`
+
+const Memoji = styled.img`
+  display: flex;
+  position: relative;
+  left: -38px;
+  height: 240px;
+`
+
+const EmojiSpan = styled.span`
+  font-size: 5rem;
+  position: relative;
+  left: -76px;
+`
+
+const Title = styled(H1)`
+  font-size: 3.5rem;
+`
+
+const Form = styled.div`
   display: flex;
   width: 100%;
   flex-direction: column;
   margin: 0 auto;
+  ${fadeInAnimation}
+`
+
+const InputWrapper = styled.div`
+  display: grid;
+  gap: 12px;
+  width: 100%;
+  margin-bottom: 50px;
 `
 
 const Label = styled.label`
   font-size: 1.5rem;
-  margin-bottom: 12px;
+  font-weight: 600;
 `
 
 const Input = styled.input`
-  margin-bottom: 50px;
+  font-size: 1.2rem;
   width: 90%;
   padding: 10px;
   height: 30px;
-  align-items: start;
+  border: 1.5px solid rgb(0, 147, 233);
+  border-radius: 8px;
+
+  &:focus {
+    outline: 5px solid #80d0c7;
+    border: none;
+  }
 `
 
-const MessageInput = styled(Input)`
-  height: 100px;
+const MessageInput = styled.textarea`
+  font-size: 1.2rem;
+  width: 90%;
+  padding: 10px;
+  height: 150px;
+  border: 1.5px solid rgb(0, 147, 233);
+  border-radius: 8px;
+
+  &:focus {
+    outline: 5px solid #80d0c7;
+    border: none;
+  }
+`
+
+const ErrorWrapper = styled.div`
+  height: 16px;
+`
+
+const ErrorText = styled.p`
+  color: red;
+  margin: 0;
 `
 
 const ButtonWrapper = styled.div`
