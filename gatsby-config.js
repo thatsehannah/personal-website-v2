b8a8144cc34ee5788c76a5652fd13b3200480574
E. Hannah <echannah631@gmail.com>
2023-06-26 10:37:42 -0500

    no dirname?

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `${__dirname}/static/images/icons/favicon-32x32.png`, // This path is relative to the root of the site.
+        icon: `static/images/icons/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
   ],
