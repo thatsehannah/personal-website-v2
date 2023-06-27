583ba35701caa08f0baf33c8cd3741ba7f8213df
E. Hannah <echannah631@gmail.com>
2023-06-27 01:47:38 -0500

    gatsby manifest changes, changed favicon, added theme borders to strength cards

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,15 +65,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `ehannah-personal-site`,
         short_name: `starter`,
-        start_url: `/`,
+        start_url: `/about`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `static/icons/favicon-32x32.png`, // This path is relative to the root of the site.
+        icon: `static/icons/apple-touch-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
